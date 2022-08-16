930c373f6a0e1dfff37bfa338d1992c685a64db7
kazemizu <kazemizu1@gmail.com>
2022-08-16 21:34:22 +0700

    master: Add new related items.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="w-full h-screen flex justify-center">
+      <Navbar />
+    </div> 
+  );
+}
+
+export default App;
